8efc4ea013b55a9c89e7085ba8a06bd57777ae61
Bhushan Goel <bgoel1991@gmail.com>
2013-06-07 12:20:24 +0530

    cmt1

---
+++ build/jst.js
@@ -0,0 +1,9 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["src/template.html"] = function(obj) {
+var __t, __p = '', __e = _.escape;
+__p += '<head><title>' +
+((__t = ( obj.title )) == null ? '' : __t) +
+'</title></head>\n';
+return __p
+};
